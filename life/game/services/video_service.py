eabd1308e856c3be898c7e3c51fe433e0e09bdfb
Austin-Earl-Private <the.qmind2020@gmail.com>
2022-10-19 22:03:27 -0600

    dump

--- life/game/services/video_service.py
+++ life/game/services/video_service.py
@@ -1,22 +1,21 @@
 import pyray
 
+from life import constants
+from life.game.shared.color import Color
+from life.game.shared.point import Point
+
 
 class VideoService:
     """Outputs the game state. The responsibility of the class of objects is to draw the game state 
     on the screen. 
     """
 
-    def __init__(self, caption, width, height, cell_size, frame_rate, debug = False):
+    def __init__(self, debug=False):
         """Constructs a new VideoService using the specified debug mode.
         
         Args:
             debug (bool): whether or not to draw in debug mode.
         """
-        self._caption = caption
-        self._width = width
-        self._height = height
-        self._cell_size = cell_size
-        self._frame_rate = frame_rate
         self._debug = debug
 
     def close_window(self):
@@ -29,64 +28,48 @@ class VideoService:
         """
         pyray.begin_drawing()
         pyray.clear_background(pyray.BLACK)
-        if self._debug == True:
-            self._draw_grid()
-    
-    def draw_actor(self, actor):
+        # if self._debug == True:
+
+    def draw_actor(self, actor, centered=False):
         """Draws the given actor's text on the screen.
 
         Args:
             actor (Actor): The actor to draw.
-        """ 
+        """
         text = actor.get_text()
         x = actor.get_position().get_x()
         y = actor.get_position().get_y()
         font_size = actor.get_font_size()
         color = actor.get_color().to_tuple()
 
+        if centered:
+            width = pyray.measure_text(text, font_size)
+            offset = int(width / 2)
+            x -= offset
+
         if actor.is_square():
-            pyray.draw_rectangle(x,y,self.get_cell_size(),self._cell_size,color)
+            pyray.draw_rectangle(x, y, constants.CELL_SIZE, constants.CELL_SIZE, color)
         else:
             pyray.draw_text(text, x, y, font_size, color)
-        
-    def draw_actors(self, actors):
+
+    def draw_line(self, point1: Point, point2: Point, color: Color):
+        pyray.draw_line(point1.get_x(), point1.get_y(), point2.get_x(), point2.get_y(), color.to_tuple())
+
+    def draw_actors(self, actors, centered=False):
         """Draws the text for the given list of actors on the screen.
 
         Args:
             actors (list): A list of actors to draw.
-        """ 
+        """
         for actor in actors:
-            self.draw_actor(actor)
-    
+            self.draw_actor(actor, centered)
+
     def flush_buffer(self):
         """Copies the buffer contents to the screen. This method should be called at the end of
         the game's output phase.
-        """ 
+        """
+
         pyray.end_drawing()
-
-    def get_cell_size(self):
-        """Gets the video screen's cell size.
-        
-        Returns:
-            Grid: The video screen's cell size.
-        """
-        return self._cell_size
-
-    def get_height(self):
-        """Gets the video screen's height.
-        
-        Returns:
-            Grid: The video screen's height.
-        """
-        return self._height
-
-    def get_width(self):
-        """Gets the video screen's width.
-        
-        Returns:
-            Grid: The video screen's width.
-        """
-        return self._width
 
     def is_window_open(self):
         """Whether or not the window was closed by the user.
@@ -102,12 +85,19 @@ class VideoService:
         Args:
             title (string): The title of the window.
         """
-        pyray.init_window(self._width, self._height, self._caption)
-        pyray.set_target_fps(self._frame_rate)
+        pyray.init_window(constants.MAX_X, constants.MAX_Y, constants.CAPTION)
+        pyray.set_target_fps(constants.FRAME_RATE)
 
-    def _draw_grid(self):
+    def draw_grid(self):
         """Draws a grid on the screen."""
-        for y in range(0, self._height, self._cell_size):
-            pyray.draw_line(0, y, self._width, y, pyray.GRAY)
-        for x in range(0, self._width, self._cell_size):
-            pyray.draw_line(x, 0, x, self._height, pyray.GRAY)
+        y_max = constants.MAX_Y - (constants.CELL_SIZE * 2) + 1
+        y_max = (constants.ROWS * constants.CELL_SIZE) + 1
+        for y in range(0, y_max, constants.CELL_SIZE):
+            pyray.draw_line(0, y, constants.MAX_X, y, pyray.GRAY)
+
+        for x in range(0, constants.MAX_X, constants.CELL_SIZE):
+            pyray.draw_line(x, 0, x, y_max, pyray.GRAY)
+
+    def _get_x_offset(self, text, font_size):
+        width = pyray.measure_text(text, font_size)
+        return int(width / 2)
